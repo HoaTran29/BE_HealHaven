71c6e74eec22376cf68e8e2397210d659a8433ce
chinhnguyen-eastgate <hoatlkse183433@fpt.edu.vn>
2026-01-30 12:23:45 +0700

    start BE

---
+++ src/main/java/backend/healhaven/HealhavenApplication.java
@@ -0,0 +1,13 @@
+package backend.healhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealhavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealhavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/healhaven/HealhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.healhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealhavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
